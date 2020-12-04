de55677afb9f29048894113d923742dacfa6550f
FraCav99 <MasterCaster36@outlook.it>
2020-12-04 10:55:48 +0100

    Fix small issue in index.html and apply logic for modals in view and controller

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,19 @@
 export default function controller(view, model) {
     const DOM = {
-        darkModeSwitch: document.querySelector('input[type="checkbox"]')
+        darkModeSwitch: document.querySelector('input[type="checkbox"]'),
+        addNewListBtn: document.getElementById('add__todo'),
+        modalContainer: document.querySelector('.modal-container'),
+        listTitleModal: document.querySelector('.title-input'),
+        discardBtn: document.getElementById('discard'),
+        addBtn: document.getElementById('add')
     }
 
+    // Toggle dark mode
     DOM.darkModeSwitch.addEventListener('click', () => view.toggleDarkMode(DOM.darkModeSwitch));
+
+    // Show modal container and its modal
+    DOM.addNewListBtn.addEventListener('click', () => view.showModal(DOM.modalContainer, DOM.listTitleModal));
+
+    // Close modal container and its modal
+    DOM.discardBtn.addEventListener('click', ev => view.closeModal(ev, DOM.modalContainer, DOM.listTitleModal))
 }
--- src/view.js
+++ src/view.js
@@ -1,5 +1,5 @@
 export default function view() {
-    
+
     const toggleDarkMode = switcher =>  {
         const darkStylesheet = document.getElementById('dark');
         switcher.checked ?
@@ -7,7 +7,25 @@ export default function view() {
         darkStylesheet.rel = 'stylesheet alternate';
     }
 
+    const showModal = (mainModalContainer, modal) => {
+        // Make modal container and its modal visible
+        mainModalContainer.classList.remove('visible');
+        modal.classList.remove('visible');
+    }
+
+    const closeModal = (ev, mainModalContainer, modal) => {
+        // Make modal container and its modal invisible
+        mainModalContainer.classList.add('visible');
+        modal.classList.add('visible');
+
+        // Prevent normal behaviour of form
+        // when buttons are clicked
+        ev.preventDefault();
+    }
+
     return {
-        toggleDarkMode
+        toggleDarkMode,
+        showModal,
+        closeModal
     }
 }
