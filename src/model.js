3d9f86827e3a64786c695eceb6180e9b2bb3b0d8
FraCav99 <MasterCaster36@outlook.it>
2020-12-05 16:46:45 +0100

    Add new todo logic implemented

--- src/model.js
+++ src/model.js
@@ -8,8 +8,33 @@ export default function model() {
         localStorage.removeItem(title);
     }
 
+    const addTodo = (
+        currentList,
+        title,
+        priority,
+        description
+        ) => {
+            // Get the array elements from the current list
+            const tempList = JSON.parse(localStorage.getItem(currentList));
+
+            // Create the new todo
+            const newTodo = {
+                title,
+                date,
+                priority,
+                description
+            }
+
+            // Push the new todo inside the array
+            tempList.push(newTodo);
+
+            // Pass the temp array and be setted to currentList
+            localStorage.setItem(JSON.stringify(tempList));
+        }
+
     return {
         createList,
-        deleteList
+        deleteList,
+        addTodo
     }
 }
